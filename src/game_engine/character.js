92c7cdf3d3ef22ff1e2e719b70ac0dd90a41f504
hackathon <hackathon@Jakes-MacBook-Air.local>
2024-08-22 14:42:49 +0100

    Fixes after merge

--- src/game_engine/character.js
+++ src/game_engine/character.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import {CHAR_HEIGHT, GAME_HEIGHT} from "@/constants/game";
 
-const JUMP_HEIGHT_PX = 100;
+const JUMP_HEIGHT_PX = 150;
 
 const useCharacterMovement = ({
     jump,
@@ -58,11 +58,11 @@ export default function useCharacter() {
     const [bottomPosition, setBottomPosition] = useState(0);
     const [spacePressed, setSpacePressed] = useState(false);
 
-    const updatedCharCoords = (GAME_HEIGHT-CHAR_HEIGHT) - charCoords.y;
+    const updatedCharCoords = charCoords.y;
     
     const handleJumpClick = () => {
         // Increase the bottom position by JUMP_HEIGHT_PX
-        setBottomPosition(updatedCharCoords + JUMP_HEIGHT_PX)
+        setBottomPosition(window.innerHeight - updatedCharCoords + JUMP_HEIGHT_PX)
     }
 
     const handleComeDown = () => {
